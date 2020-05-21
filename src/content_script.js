300b518af6bb9b28c7786609e9cad506e247e0e5
Sergey Ermolaev <s.ermolaev@solvemate.com>
2020-05-21 22:31:55 +0400

    Refactor content_script

--- src/content_script.js
+++ src/content_script.js
@@ -1,67 +1,69 @@
-const throttle = (callback, limit) => {
-  let wait = false;
-  return function () {
-    if (!wait) {
-      callback.call();
-      wait = true;
-      setTimeout(() => {wait = false}, limit);
-    }
-  }
-}
+class ContentScript {
+  constructor() {
+    let location = window.location;
+    this.matchURL = 'app.slack.com';
+    this.KEY = 'l';
+    this.activationListenerActivated = false;
+    this.hostname = location.hostname;
+    this.chatId = location.pathname.split('/')[2];
+    this.usersList = [];
+    this.messagesHolder = document.querySelector('body');
+    this.MARK_MODE = false;
+    this.clickFn = this.singeClick.bind(this);
 
-const getMainParent = (elem) => {
-  return elem
-    .parentElement.parentElement.parentElement
-    .parentElement.parentElement.parentElement;
-}
+    this.clickOptions = {
+      once: true,
+      passive: false,
+      capture: true
+    };
 
-const deleteUsedDetection = (oldValues, newValues, key) => {
-  if (!oldValues || !newValues) return [];
+    this.mutationConfig = {
+      attributes: false,
+      characterData: false,
+      childList: true
+    };
 
-  return oldValues.filter(firstArrayItem =>
-    !newValues.some(
-      secondArrayItem => firstArrayItem[key] === secondArrayItem[key]
-    )
-  );
-};
-
-const cleanUpDeletedUser = (deletedUsers) => {
-  deletedUsers.forEach(user => markUser(user, true));
-}
-
-const storageUpdate = (chatId, cb) => {
-  const saveToStorage = (userList, chatId) => (newUser) => {
-    userList.unshift({color: '#FF0000', ignore: false, ...newUser});
-    chrome.storage.sync.set({ [chatId]: userList });
+    this.addObserver();
   }
 
-  chrome.storage.onChanged.addListener((changes) => {
-    const storageChange = changes[chatId];
-    if (storageChange) {
-      let deletedUsers = deleteUsedDetection(storageChange.oldValue, storageChange.newValue, 'id');
-      cleanUpDeletedUser(deletedUsers);
-      let updatedList = storageChange.newValue;
-      cb(updatedList, saveToStorage(updatedList, chatId));
+  addObserver() {
+    if (this.hostname === this.matchURL && this.chatId) {
+      this.storageUpdate(() => {
+
+        if (this.observer) {
+          this.observer.disconnect();
+          this.observer = null;
+        }
+        this.observer = this.watchDOMChanges();
+        if (!this.activationListenerActivated) {
+          this.activationListenerActivated = true;
+          window.addEventListener("keydown",
+            (e) => this.onKeyCombination(e));
+        }
+      });
     }
-  });
+  }
 
-  chrome.storage.sync.get([chatId], (result) => {
-    let updatedList = result[chatId] || [];
-    cb(updatedList, saveToStorage(updatedList, chatId));
-  });
-}
+  onKeyCombination(e) {
+    if (e.ctrlKey && e.shiftKey && e.metaKey && e.key.toLowerCase() === this.KEY) {
+      this.messagesHolder.removeEventListener('click', this.clickFn, this.clickOptions);
 
-const KEY = 'l';
-let MARK_MODE_ON = false;
-const onKeyCombination = (e, saveToStorage) => {
-  const messageSelector = 'a.c-message__sender_link';
-  const messageHolderSelector = '.p-message_pane.p-message_pane--classic-nav';
+      if (!this.MARK_MODE) {
+        this.messagesHolder.setAttribute('style', 'border: 6px dashed red');
+        this.messagesHolder.addEventListener('click', this.clickFn, this.clickOptions);
+        this.MARK_MODE = true;
+      } else {
+        this.messagesHolder.setAttribute('style', '');
+        this.MARK_MODE = false;
+      }
+    }
+  }
 
-  function fn (event) {
+  singeClick(event) {
+    const messageSelector = 'a.c-message__sender_link';
+    this.messagesHolder.setAttribute('style', '');
+
     if (!event.target.matches(messageSelector)) {
-      MARK_MODE_ON = false;
-      this.setAttribute('style', '');
-      this.removeEventListener('click', arguments.callee, false);
       return;
     }
     event.stopImmediatePropagation();
@@ -73,94 +75,97 @@ const onKeyCombination = (e, saveToStorage) => {
       const user = target.innerText;
 
       if (userId) {
-        saveToStorage({id: userId, name: user});
+        this.saveToStorage({id: userId, name: user});
       }
     }
-
-    MARK_MODE_ON = false;
-    this.setAttribute('style', '');
-    this.removeEventListener('click', arguments.callee, false);
+    this.MARK_MODE = false;
   }
 
-  if (e.ctrlKey && e.shiftKey && e.metaKey && e.key.toLowerCase() === KEY) {
-    MARK_MODE_ON = !MARK_MODE_ON;
+  watchDOMChanges() {
+    const messagesBody = document.querySelectorAll('body')[0];
+    const updateStyles = () => this.usersList.forEach(user => this.markUser(user, false));
+    const observer = new MutationObserver(()  => this.throttle(updateStyles(this.usersList), 100));
 
-    //const messagesHolder =  document.querySelectorAll(messageHolderSelector)[0];
-    const messagesHolder =  document.querySelector('body');
-    if (MARK_MODE_ON) {
-      messagesHolder.setAttribute('style', 'border: 6px dashed red');
-      messagesHolder.addEventListener('click', fn, false);
-    } else {
-      messagesHolder.setAttribute('style', '');
-    }
+    observer.observe(messagesBody, this.mutationConfig);
+    updateStyles(this.usersList);
+
+    return observer;
   }
-}
 
-const markUser = (user, clearStyles) => {
-  let elements = Array.from(document.querySelectorAll(`[data-message-sender="${user.id}"]`));
-
-  if (elements) {
-    elements.forEach( (userNameContainer) => {
-      const message = getMainParent(userNameContainer);
-      // const container = message.parentElement;
-
-      if (clearStyles) {
-        message.setAttribute('style', '');
-      } else {
-        message.setAttribute('style', `background: ${user.color}`);
-        if (user.ignore) {
-          message.setAttribute("style", `display: none`);
-        }
-      }
-    })
-  }
-}
-
-const watchDOMChanges = (list) => {
-  const scrollContainerSelector = 'body';
-  const config = {
-    attributes: false,
-    characterData: false,
-    childList: true
-  };
-
-  // 0 - navigation panel, 1 - messages list
-  const messagesBody = document.querySelectorAll(scrollContainerSelector)[0];
-  const updateStyles = () => list.forEach(user => markUser(user, false));
-  const observer = new MutationObserver(()  => throttle(updateStyles(list), 100));
-
-  observer.observe(messagesBody, config);
-  updateStyles(list);
-
-  return observer;
-}
-
-let USER_LIST = [];
-const init = () => {
-  const matchURL = "app.slack.com";
-
-  const location = window.location;
-  const hostname = location.hostname;
-  const chatId = location.pathname.split('/')[2];
-  let activationListener;
-  let observer;
-
-
-  if (hostname === matchURL && chatId) {
-    storageUpdate(chatId, (list, saveToStorage) => {
-      USER_LIST =  list;
-
-      if (observer) {
-        observer.disconnect();
-        observer = null;
-      }
-      observer = watchDOMChanges(list);
-      if (!activationListener) {
-        activationListener = window.addEventListener("keydown",
-          (e) => onKeyCombination(e, saveToStorage));
+  storageUpdate(cb) {
+    chrome.storage.onChanged.addListener((changes) => {
+      const storageChange = changes[this.chatId];
+      if (storageChange) {
+        let deletedUsers = this.deletedUsedDetection(storageChange.oldValue, storageChange.newValue, 'id');
+        this.cleanUpDeletedUser(deletedUsers);
+        this.usersList = storageChange.newValue;
+        cb(this.usersList);
       }
     });
+
+    chrome.storage.sync.get([this.chatId], (result) => {
+      this.usersList = result[this.chatId] || [];
+      cb(this.usersList);
+    });
+  }
+
+  saveToStorage(newUser) {
+    if ( !this.usersList.some(u => u.id === newUser.id) ) {
+      this.usersList.unshift({color: '#FF0000', ignore: false, ...newUser});
+      chrome.storage.sync.set({ [this.chatId]: this.usersList });
+    }
+  }
+
+  cleanUpDeletedUser(deletedUsers) {
+    deletedUsers.forEach(user => this.markUser(user, true));
+  }
+
+  markUser(user, clearStyles) {
+    let elements = Array.from(document.querySelectorAll(`[data-message-sender="${user.id}"]`));
+
+    if (elements) {
+      elements.forEach( (userNameContainer) => {
+        const message = this.getMainParent(userNameContainer);
+
+        if (clearStyles) {
+          message.setAttribute('style', '');
+        } else {
+          message.setAttribute('style', `background: ${user.color}`);
+          if (user.ignore) {
+            message.setAttribute("style", `display: none`);
+          }
+        }
+      })
+    }
+  }
+
+  // Helpers
+
+  deletedUsedDetection(oldValues, newValues, key) {
+    if (!oldValues || !newValues) return [];
+    return oldValues.filter(firstArrayItem =>
+      !newValues.some(
+        secondArrayItem => firstArrayItem[key] === secondArrayItem[key]
+      )
+    );
+  }
+
+  getMainParent(elem) {
+    return elem
+      .parentElement.parentElement.parentElement
+      .parentElement.parentElement.parentElement;
+  }
+
+  throttle(callback, limit) {
+    let wait = false;
+    return function () {
+      if (!wait) {
+        callback.call();
+        wait = true;
+        setTimeout(() => {wait = false}, limit);
+      }
+    }
   }
 }
 
-window.addEventListener('load', () => init(), false);
+window.addEventListener('load', () => new ContentScript(), false);
